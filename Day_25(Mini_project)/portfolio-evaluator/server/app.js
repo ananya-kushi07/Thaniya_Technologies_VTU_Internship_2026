dff466510c1fa06159b19016295bf7984e16bbd7
ananya-kushi07 <ananya.kushi07@gmail.com>
2026-03-22 10:00:00 +0530

    Day 1:Started Portfolio- evaluator, Backend setup with Express server and MongoDB connection

---
+++ Day_25(Mini_project)/portfolio-evaluator/server/app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+// Load env FIRST
+dotenv.config();
+
+// Connect DB
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
+
+// Test API route
+app.get("/api/test", (req, res) => {
+  res.json({ message: "Backend working ✅" });
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
